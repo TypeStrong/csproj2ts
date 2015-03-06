6fbbd4044633edd556538ca8613b38a0b7661d3d
Steve Ognibene <steve.ognibene@outlook.com>
2015-03-05 22:49:22 -0500

    basic docs and a demo JS file.

---
+++ demo.js
@@ -0,0 +1,12 @@
+﻿var csproj2ts = require('./csproj2ts');
+
+var vsProjInfo = {
+    ProjectFileName: "csproj2ts.csproj",
+    ActiveConfiguration: "Debug"
+}
+
+csproj2ts.getTypeScriptSettings(vsProjInfo).then(function (settings) {
+    console.log(settings);
+}).catch(function (error) {
+    console.log(error);
+});
